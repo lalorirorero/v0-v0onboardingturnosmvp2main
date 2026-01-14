6b711cf6d1d3aba4849dd2c39163eafe6a870baf
lalorirorero <edugosepu@gmail.com>
2026-01-14 09:54:54 -0300

    Actualizar titulo de la pagina

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ const _geist = Geist({ subsets: ["latin"] })
 const _geistMono = Geist_Mono({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: "v0 App",
+  title: "GeoVictoria | Configuracion Inicial",
   description: "Created with v0",
   generator: "v0.app",
   icons: {
