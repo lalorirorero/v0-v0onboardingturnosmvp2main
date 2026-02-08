0a81779f2dbf998439bc673262d984c8ce08694d
lalorirorero <edugosepu@gmail.com>
2026-02-08 19:04:08 -0300

    Remove video modal header close icon

--- components/onboarding-turnos.tsx
+++ components/onboarding-turnos.tsx
@@ -1314,20 +1314,6 @@ const TrabajadoresStep = ({
                   Tutorial paso a paso para importar trabajadores
                 </DialogDescription>
               </div>
-              <button
-                type="button"
-                onClick={() => setShowVideoModal(false)}
-                className="rounded-full p-2 hover:bg-slate-100 transition-colors"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                  className="w-5 h-5 text-slate-600"
-                >
-                  <path d="M6.28 5.22a.75.75 0 00-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 101.06 1.06L10 11.06l3.72 3.72a.75.75 0 101.06-1.06L10 8.94 6.28 5.22z" />
-                </svg>
-              </button>
             </DialogHeader>
 
             <div className="p-6 space-y-4">
